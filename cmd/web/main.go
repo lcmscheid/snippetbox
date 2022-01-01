619bd4f1c55274baeca23e01e667ba44c8c50d2d
Luiz Carlos Scheid <lcmscheid@gmail.com>
2022-01-01 19:28:53 +0100

    wip

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,15 +1,19 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type Config struct {
 	Addr      string
 	StaticDir string
+	dsn       string
 }
 
 type application struct {
@@ -22,6 +26,7 @@ func main() {
 	cfg := new(Config)
 	flag.StringVar(&cfg.Addr, "addr", ":4000", "HTTP network address")
 	flag.StringVar(&cfg.StaticDir, "static-dir", "./ui/static", "Path to static assets")
+	flag.StringVar(&cfg.dsn, "dsn", "web:manager@/snippetbox?parseTime=true", "MySQL data source name")
 	flag.Parse()
 
 	app := &application{
@@ -30,6 +35,12 @@ func main() {
 		config:   cfg,
 	}
 
+	db, err := openDb(cfg.dsn)
+	if err != nil {
+		app.errorLog.Fatal(err)
+	}
+	defer db.Close()
+
 	srv := &http.Server{
 		Addr:     app.config.Addr,
 		ErrorLog: app.errorLog,
@@ -37,6 +48,17 @@ func main() {
 	}
 
 	app.infoLog.Printf("Starting server on %s", cfg.Addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	app.errorLog.Fatal(err)
+}
+
+func openDb(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
 }
